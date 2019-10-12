91a9f1912ccb89da285e943f236b23dff4efea49
arturol76 <arturo.lotito@gmail.com>
2019-10-12 15:04:18 +0200

    wip

--- run.sh
+++ run.sh
@@ -4,12 +4,12 @@ show_help()
     echo ------------------------------------------------------------------
     echo Runs "arturol76/phusion-baseimage" on the target "docker-ip" host.
     echo Container name is specified by "container_name".
-    echo Container will expose SSH service on port "docker_port".
+    echo Container will expose SSH service on port "ssh_docker_port".
     echo
     echo NOTE: it will create and mount a named volume for the persistent storage of .ssh files.
     echo
     echo USAGE:
-    echo ./run.sh docker_ip container_name docker_port
+    echo ./run.sh docker_ip container_name ssh_docker_port
     echo
     echo EXAMPLE:
     echo ./run.sh 192.168.2.96 phusion-baseimage-test 6022
@@ -19,7 +19,7 @@ show_help()
 
 num_of_params=3
 docker_host=$1
-docker_name=$2
+container_name=$2
 docker_port=$3
 
 #checks number of parameters
@@ -37,18 +37,18 @@ docker_image=arturol76/phusion-baseimage
 #--------------------------------------
 
 echo creating volumes...
-volume_ssh=${docker_name}_ssh
+volume_ssh=${container_name}_ssh
 docker -H $docker_host volume create ${volume_ssh}
 
 #if docker is already running, stops it
-if [ "$(docker -H $docker_host ps -a | grep $docker_name)" ]; then
+if [ "$(docker -H $docker_host ps -a | grep $container_name)" ]; then
     echo container $docker_host already exists
 
     echo stopping it...
-    docker -H $docker_host stop $docker_name
+    docker -H $docker_host stop $container_name
 
     echo removing it...
-    docker -H $docker_host rm $docker_name
+    docker -H $docker_host rm $container_name
 fi
 
 # create your container
@@ -58,12 +58,12 @@ docker -H $docker_host create \
     -p ${docker_port}:22/tcp \
     -e TZ="Europe/Rome" \
     -v ${volume_ssh}:"/root/.ssh":rw \
-    --name $docker_name \
+    --name $container_name \
     $docker_image
 
 #run
 echo starting the container...
 docker -H $docker_host start \
-    $docker_name
+    $container_name
 
 exit 0
